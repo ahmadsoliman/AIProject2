0288d178ac241ba7499b8f51d8dd08400a440a71
Ahmad Soliman <ahmadadel92@gmail.com>
2014-12-15 01:02:06 +0200

    dead

--- planner.py
+++ planner.py
@@ -12,9 +12,11 @@ class action:
     def __repr__(self):
         return self.name.__repr__()
     def __eq__(self, other):
-        return self.name.eq(other.name) and sorted(self.preconds) == sorted(other.preconds) and sorted(self.effects) == sorted(other.preconds)
-    def __hash__(self):
-        return id(self)
+        return self.name.eq(other.name) and arrayEq(sorted(self.preconds), sorted(other.preconds)) and arrayEq(sorted(self.effects), sorted(other.preconds))
+    def __ne__(self, other):
+        return not(self.eq(other))
+    #def __hash__(self):
+    #    return self.__repr__().__hash__()
     def clone(self, fresh=True):
         dic = {}
         def assignVar(vname):
@@ -37,8 +39,15 @@ class action:
         neweffects = [replaceNames(e.clone()) for e in self.effects]
         return action(newname, newpreconds, neweffects)
 
+def arrayEq(a1, a2):
+    if len(a1)!=len(a2):
+        return False
+    for b1,b2 in itertools.izip(a1,a2):
+        if not unify(b1, b2).found: return False
+    return True
+
+
 def inArray(action, actions, sol):
-    print sol
     action = action.clone(False)
     action.preconds = [subst(sol, precond) for precond in action.preconds]
     action.effects = [subst(sol, effect) for effect in action.effects]
@@ -57,15 +66,15 @@ aInf = None
 
 def POP(O, s0, g):
     global ops, a0, aInf
-    ops = O
     a0 = action(const('start'), [], s0)
     aInf = action(const('finish'), g, [])
     ops.append(a0)
     ops.append(aInf)
+    ops += O
     agenda = []
     for precond in g:
         agenda.append((aInf, precond))
-    return POP1(([a0, aInf], [], set([(a0, aInf)]), solution(True)), agenda)
+    return POP1(([a0, aInf], [], [(a0, aInf)], solution(True)), agenda)
 
 # pi is a 4-tuple <A,L,Ord,B>
 
@@ -75,11 +84,11 @@ def POP1(PI, agenda):
 
     A, L, Ord, B = PI
     # expand pi tuple
-    print 'A: ', A
+    #print 'A: ', A
     # print 'L: ', L
     # print 'Ord: ', Ord
     # print 'B: ', B
-    print 'agenda: ', agenda
+    #print 'agenda: ', agenda
 
     # if (agenda = phi) then return pi
     if len(agenda) == 0:
@@ -95,12 +104,12 @@ def POP1(PI, agenda):
                 A, L, Ord, B = PI
                 A = [a.clone(False) for a in A]
                 L = [(l[0].clone(False), l[1].clone(), l[2].clone(False)) for l in L]
-                Ord = set([(l[0].clone(False), l[1].clone(False)) for l in Ord])
+                Ord = [(l[0].clone(False), l[1].clone(False)) for l in Ord]
                 B = B.clone()
                 PI = (A, L, Ord, B)
 
                 aj = op
-                print 'aj: : : : : : : :: : ', aj
+                #print 'aj: : : : : : : :: : ', aj
                 sol = unify(effect, pi)
                 # if achievers = [] return failure
                 if aj is None:
@@ -109,7 +118,9 @@ def POP1(PI, agenda):
                 # L = L union { aj --pi--> ai }
                 L.append((aj, pi, ai))
                 # update Ord with aj < ai
-                Ord.add((aj, ai))
+                if (aj, ai) not in Ord:
+                    Ord.append((aj, ai))
+
                 # Update B with binding constraints of this link
                 if not B.consistent(sol):
                     continue
@@ -119,14 +130,18 @@ def POP1(PI, agenda):
                     # A = A union {aj}
                     A.append(aj)
                     # update Ord with a0 < aj and aj < aInf
-                    Ord.add((a0, aj))
-                    Ord.add((aj, aInf))
+                    if not(a0.name==aj.name):
+                        if (a0, ai) not in Ord:
+                            Ord.append((a0, aj))
+                    if not(aj.name==aInf.name):
+                        if (aj, aInf) not in Ord:
+                            Ord.append((aj, aInf))
                     # add all preconditions of aj to the agenda
                     for precond in aj.preconds:
                         agenda.append((aj, precond))
                 # resolve new threats
                 #PI = RESOLVE_THREATS((A, L, Ord, B), aj, (aj, pi, ai))
-                resPI = POP1(PI, agenda)
+                resPI = POP1((A, L, Ord, B), agenda)
                 if resPI is not None: return resPI
 
 def RESOLVE_THREATS(PI, al, l):
@@ -142,22 +157,79 @@ def RESOLVE_THREATS(PI, al, l):
                         if not sol.consistent(B):
                             sol = None
                             continue
-                        Ord1 = copy(Ord)
-                        Ord1.add((ai, ak))
-                        Ord1.add((ak, aj))
+                        Ord1 = [(l[0].clone(False), l[1].clone(False)) for l in Ord]
+                        if (ai, ak) not in Ord:
+                            Ord1.append((ai, ak))
+                        if (ak, aj) not in Ord:
+                            Ord1.append((ak, aj))
                         if not consistentOrder(Ord1):
                             sol = None
                             continue
                         break
                 if sol:
-                    Ord.add((ak, ai))
+                    if (ak, ai) not in Ord:
+                        Ord.append((ak, ai))
     return (A, L, Ord, B)
 
 def consistentOrder(pairs):
+    nodec = 0
+    nodem = []
+    for a,b in pairs:
+        if a not in nodem:
+            nodem.append(a)
+        if b not in nodem:
+            nodem.append(b)
+    graph = [[]]*len(nodem)
+    for a,b in pairs:
+        x=nodem.index(a)
+        y=nodem.index(b)
+        if y not in graph[x]: graph[x].append(y)
+        if x not in graph[y]: graph[y].append(x)
+    return dfs(graph, set([]), 0, -1)
+
+def dfs(graph, vis, n, p):
+    if n in vis:
+        return False
+    vis.add(n)
+    for adj in graph[n]:
+        if adj != p:
+            if not dfs(graph, vis, adj, n):
+                return False
     return True
 
 
+def linearize(pairs, sol):
+    nodec = 0
+    nodem = []
+    for a,b in pairs:
+        if a.name not in nodem:
+            nodem.append(a.name)
+        if b.name not in nodem:
+            nodem.append(b.name)
+    graph = [[]]*len(nodem)
+    for a,b in pairs:
+        x=nodem.index(a.name)
+        y=nodem.index(b.name)
+        if y not in graph[x]: graph[x].append(y)
+        if x not in graph[y]: graph[y].append(x)
+    global a0
+    res = []
+    top_sort(graph, set([]), nodem.index(a0.name), res)
+    items = []
+    res = reversed(res)
+    for item in res:
+        nodem[item] = subst(sol, nodem[item])
+        nodem[item] = subst(sol, nodem[item])
+        nodem[item] = subst(sol, nodem[item])
+        items.append(nodem[item])
+    return items
 
+def top_sort(graph, vis, node, res):
+    vis.add(node)
+    for adj in graph[node]:
+        if not adj in vis:
+            top_sort(graph, vis, adj, res)
+    res.append(node)
 
 # test
 
@@ -186,4 +258,8 @@ O = [
         )
 ]
 
-print POP(O, s0, g)
+plan = POP(O, s0, g)
+
+print plan
+
+print linearize(plan[2], plan[3])
